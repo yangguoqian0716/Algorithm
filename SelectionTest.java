cf2c9bedb99bffaa7ccd7c9e84500bd2e8b07bed
yangguoqian0716 <105093945+yangguoqian0716@users.noreply.github.com>
2023-02-17 14:07:53 +0800

    Add files via upload

---
+++ SelectionTest.java
@@ -0,0 +1,38 @@
+package SelectionSort;
+
+import java.util.Arrays;
+
+public class SelectionTest {
+    public static void main(String[] args){
+        Integer[] arr ={3,5,1,2,6,9,8,4,7};
+        Selection.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+class Selection {
+    public static void sort(Comparable[] a){
+        for(int i=0;i<a.length-2;i++){
+            int minIndex=i;
+
+            for(int j=i+1;j<a.length;j++){
+                if(greater(a[minIndex],a[j])){
+                    minIndex=j;
+                }
+            }
+            ex(a,i,minIndex);
+
+        }
+    }
+    public static boolean greater(Comparable v,Comparable w){
+        return v.compareTo(w)>0;
+    }
+    public static void ex(Comparable[] a,int i,int j){
+        Comparable temp;
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+    }
+
+}
