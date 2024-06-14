7815b81708ed6c4dba27afc45916be716f5e3aa2
yangguoqian0716 <105093945+yangguoqian0716@users.noreply.github.com>
2024-06-14 15:46:32 +0800

    Create bubbleSort.c

---
+++ bubbleSort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// 冒泡排序函数
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // 交换arr[j]和arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("原始数组: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    bubbleSort(arr, n);
+
+    printf("排序后的数组: \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    
+    return 0;
+}
